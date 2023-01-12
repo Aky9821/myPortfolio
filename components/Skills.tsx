2fe884089f27bbdbf63f35fd45f969a921812b97
Akshay <akshay.nagar0101@outlook.com>
2023-01-12 11:08:46 +0530

    Added api

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -1,51 +1,48 @@
 import React, { useEffect, useState } from 'react'
+import axios from "axios"
 
 import { motion } from "framer-motion";
 import Leet from './Leet';
-import { LeetStats } from './utils/LeetStats';
-import { LeetService } from './utils/LeetService';
+
 import { stat } from 'fs';
 type Props = {}
+const ENDPOINT = 'https://leetcode-stats.vercel.app'
 
-function Skills({ }: Props) {
-  interface Todo {
-    loading: boolean,
-    Stats: LeetStats[],
-    errorMsg: string
-  }
-  const [todos, setTodos] = useState<Todo>({
-    loading: false,
-    Stats: [] as LeetStats[],
-    errorMsg: ''
-  })
+
+function Skills({}: Props) {
+  const [svg, setSvg] = useState('')
+  const [theme, setTheme] = useState('Light')
+  
   useEffect(() => {
-    setTodos({ ...todos, loading: true })
-    LeetService.getStats()
-      .then(res => setTodos({
-        ...todos,loading:false, Stats:res.data
-      }))
-      .catch(err => setTodos({
-        ...todos,loading:false,errorMsg:err.message
-      }));
+    
+    const username = 'aky9821'
+    axios
+      .get(`${ENDPOINT}/api?username=${username}&theme=${theme}`)
+      .then((response) => {
+        setSvg(response.data as string)
+        
+      })
+      .catch(() => {
+       
+      })
+    
   }, [])
 
 
-  const {loading,Stats,errorMsg}=todos
-  return (
-    <div className=' flex flex-row  relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-        Skills
-      </h3>
-      <div>{
-
-        
-       
-      }
-      </div>
-
-
+return (
+  <div className=' flex flex-row  relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+    <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+      Skills
+    </h3>
+    <div>
+      <div dangerouslySetInnerHTML={{ __html: svg }} />
     </div>
-  )
+
+
+  </div>
+)
 }
 
 export default Skills
+
+
